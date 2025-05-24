4a51cdc0bf5a326efd6b86346f7544d92e20a220
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2025-05-24 10:43:32 -0500

    initial version, postgres enabled

--- config.py
+++ config.py
@@ -1,16 +1,20 @@
 import os
 
-DB_FILE_PATH = os.path.join(os.path.dirname(__file__), "notes.sqlite")
-
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{DB_FILE_PATH}"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'postgresql://flask_user:flask_password@localhost:5432/flask_notes_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "this-is-not-secret"
-
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "this-is-not-secret"
 
 class TestConfig:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///test_notes.db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://flask_user:flask_password@localhost:5432/flask_test_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "this-is-not-secret"
     TESTING = True
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+    
